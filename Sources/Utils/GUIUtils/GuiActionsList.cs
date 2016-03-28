80705339af7af7e7f85a03c5033c1658af06be7c
ihsoft <igor.zavoychinskiy@gmail.com>
2016-03-28 00:07:43 -0700

    Better comments

--- Sources/Utils/GUIUtils/GuiActionsList.cs
+++ Sources/Utils/GUIUtils/GuiActionsList.cs
@@ -8,18 +8,28 @@ using UnityEngine;
 
 namespace KSPDev.GUIUtils {
 
-/// <summary>
-/// </summary>
-// FIXME: give descriptions.
+/// <summary>A helper to accumulate GUI actions.</summary>
+/// <remarks>Unity may issue multiple GUI passes during a frame, and it requires number of UI
+/// elements not to change between the passes. When UI selection affects representation all the
+/// changes must be postponed till it's safe to do the changes. This helper can be used to store
+/// actions that will be executed at the very last pass.</remarks>
 public class GuiActionsList {
+  /// <summary>GUI action type.</summary>
   public delegate void GuiAction();
   
+  /// <summary>A list of pending actions.</summary>
   private readonly List<GuiAction> guiActions = new List<GuiAction>();
-  
+
+  /// <summary>Adds an action to the pending list.</summary>
+  /// <param name="actionFn">An action callback.</param>
   public void Add(GuiAction actionFn) {
     guiActions.Add(actionFn);
   }
-  
+
+  /// <summary>Executes actions when it's safe to do the changes.</summary>
+  /// <remarks>It's safe to call this method in every pass. It will detect when it's safe to apply
+  /// the changes and apply the changes only once per a frame.</remarks>
+  /// <returns><c>true</c> if actuions have been applied.</returns>
   public bool ExecutePendingGuiActions() {
     if (Event.current.type == EventType.Layout) {
       foreach (var actionFn in guiActions) {
--- Sources/Utils/LogUtils/Logger.cs
+++ Sources/Utils/LogUtils/Logger.cs
@@ -6,22 +6,24 @@ using System;
 
 namespace KSPDev.LogUtils {
 
-/// <summary>
-/// A development logger that respects logging level and supports string formatting.
-/// </summary>
+/// <summary>A set of convenience logging method.</summary>
 public static class Logger {
+  /// <summary>Logs a formatted message as INFO record.</summary>
   public static void logInfo(String fmt, params object[] args) {
     UnityEngine.Debug.Log(String.Format(fmt, args));
   }
 
+  /// <summary>Logs a formatted message as WARNING record.</summary>
   public static void logWarning(String fmt, params object[] args) {
     UnityEngine.Debug.LogWarning(String.Format(fmt, args));
   }
 
+  /// <summary>Logs a formatted message as ERROR record.</summary>
   public static void logError(String fmt, params object[] args) {
     UnityEngine.Debug.LogError(String.Format(fmt, args));
   }
 
+  /// <summary>Logs an exception stack trace as EXCEPTION record.</summary>
   public static void logException(Exception ex) {
     UnityEngine.Debug.LogException(ex);
   }
