5d360f43e8abd190a099209776dffaa9641d1907
ihsoft <igor.zavoychinskiy@gmail.com>
2016-04-02 19:17:13 -0700

    Extract untils into own project

---
+++ Sources/Utils/Properties/AssemblyInfo.cs
@@ -0,0 +1,29 @@
+#region Using directives
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+#endregion
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle ("KSPDev_Utils")]
+[assembly: AssemblyDescription ("A redistributable set set of client utils")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCompany ("IHSoft")]
+[assembly: AssemblyProduct ("")]
+[assembly: AssemblyCopyright ("Public domain license")]
+[assembly: AssemblyTrademark ("")]
+[assembly: AssemblyCulture ("")]
+// This sets the default COM visibility of types in the assembly to invisible.
+// If you need to expose a type to COM, use [ComVisible(true)] on that type.
+[assembly: ComVisible (false)]
+// The assembly version has following format :
+//
+// Major.Minor.Build.Revision
+//
+// You can specify all the values or you can use the default the Revision and 
+// Build Numbers by using the '*' as shown below:
+[assembly: AssemblyVersion ("1.0.0")]
+[assembly: Guid ("89e3b337-b770-4422-b56e-159590c94c9d")]
+[assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
