7c5471ed9b7a3958beb3da50f567c55f5b9f7b58
ihsoft <igor.zavoychinskiy@gmail.com>
2016-04-02 23:40:38 -0700

    Set pre-release version

--- Sources/Utils/Properties/AssemblyInfo.cs
+++ Sources/Utils/Properties/AssemblyInfo.cs
@@ -24,6 +24,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("1.0.0")]
+[assembly: AssemblyVersion ("0.9.0")]
 [assembly: Guid ("89e3b337-b770-4422-b56e-159590c94c9d")]
 [assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
