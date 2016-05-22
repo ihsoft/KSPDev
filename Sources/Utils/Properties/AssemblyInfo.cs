bff68a5986699797cf971c50957920e77e2ec189
ihsoft <igor.zavoychinskiy@gmail.com>
2016-05-21 21:17:39 -0700

    Pre-release 0.12.0

--- Sources/Utils/Properties/AssemblyInfo.cs
+++ Sources/Utils/Properties/AssemblyInfo.cs
@@ -24,8 +24,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("0.11.0")]
-[assembly: AssemblyInformationalVersion ("0.11.0 for KSP v1.1")]
-[assembly: KSPAssembly ("KSPDev_Utils", 0, 11)]
+[assembly: AssemblyVersion ("0.12.0")]
+[assembly: AssemblyInformationalVersion ("0.12.0 for KSP v1.1")]
+[assembly: KSPAssembly ("KSPDev_Utils", 0, 12)]
 [assembly: Guid ("89e3b337-b770-4422-b56e-159590c94c9d")]
 [assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
