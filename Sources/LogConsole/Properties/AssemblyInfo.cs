07c94a4860c00858c2e4c3452781b23398718b79
ihsoft <igor.zavoychinskiy@gmail.com>
2016-06-10 22:36:56 -0700

    Pre-release 0.11.1

--- Sources/LogConsole/Properties/AssemblyInfo.cs
+++ Sources/LogConsole/Properties/AssemblyInfo.cs
@@ -24,9 +24,8 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
-[assembly: AssemblyVersion ("0.11.0")]
-[assembly: AssemblyInformationalVersion("0.11.0 for KSP v1.1")]
+[assembly: AssemblyVersion ("0.11.1")]
+[assembly: AssemblyInformationalVersion("0.11.1 for KSP v1.1")]
 [assembly: KSPAssembly("KSPDev_LogConsole", 0, 10)]
-[assembly: KSPAssemblyDependency("KSPDev_Utils", 0, 10)]
 [assembly: Guid ("4a5a3eab-4159-4375-8329-4f739fcb6226")]
 [assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
