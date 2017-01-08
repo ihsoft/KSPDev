65352faf0607bd89356427828ebd3ee22296b4a8
ihsoft <igor.zavoychinskiy@gmail.com>
2017-01-08 01:56:27 -0800

    Add version logger

---
+++ Sources/Utils/VersionLogger.cs
@@ -0,0 +1,24 @@
+﻿// Kerbal Development tools.
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using System;
+using System.Reflection;
+using UnityEngine;
+using KSPDev.FSUtils;
+
+/// <summary>
+/// Debugging class that reports which DLL was actually loaded for the Utils assembly.
+/// </summary>
+/// <remarks>
+/// It helps understanding which binary is used in case of multiple mods are using same version of
+/// KSPDev_utils assembly.
+/// </remarks>
+[KSPAddon(KSPAddon.Startup.Instantly, true /*once*/)]
+class VersionLogger : MonoBehaviour {
+  void Awake() {
+    var assembly = Assembly.GetExecutingAssembly();
+    var relPath = KspPaths.MakeRelativePathToGameData(assembly.Location);
+    Debug.LogFormat("Assembly {0} is loaded from {1}", assembly.FullName, relPath);
+  }
+}
