314b438b2098d7a078237c20437552cdd7b3a311
ihsoft <igor.zavoychinskiy@gmail.com>
2017-04-27 23:15:33 -0700

    Improve PosAndRot type
    - Change  serialized format to use Euler rotations instead of up/forward
    vectors.
    - Normalize angles.
    - Add handy constructors.
    - Add Clone method.

--- Sources/Utils/Types/PosAndRot.cs
+++ Sources/Utils/Types/PosAndRot.cs
@@ -12,61 +12,107 @@ namespace KSPDev.Types {
 
 /// <summary>Type to hold position and rotation of a transform. It can be serialized.</summary>
 /// <remarks>
-/// The value serializes into a string of either 2 or 3 triples of numbers separated by a comma:
+/// The value serializes into 6 numbers separated by a comma. They form two triplets:
 /// <list type="bullet">
-/// <item>The first triple is a position: (x, y, z).</item>
+/// <item>The first triplet is a position: x, y, z.</item>
 /// <item>
-/// The second triple is a "forward" direction: (x, y, z). It's not required to be normalized.
-/// </item>
-/// <item>
-/// The optional third triple is an "upwards" direction: (x, y, z). It's not required to be
-/// normalized. If omitted then a standard <c>Vector3.up</c> value is used. For the proper
-/// orientation the upwards direction must not be the same as the forward one. Otherwise, the
-/// rotation around the forward axis will be undetermined (it will be random, basically).
+/// The second triplet is a Euler rotaion around each axis: x, y, z.
 /// </item>
 /// </list>
 /// </remarks>
-/// <seealso cref="PersistentFieldAttribute"/>
-/// <seealso cref="ConfigAccessor"/>
 public sealed class PosAndRot : IPersistentField {
   /// <summary>Position of the transform.</summary>
   public Vector3 pos;
+  
+  /// <value>Euler rotation.</value>
+  /// <remarks>
+  /// The rotation angles are automatically adjusted to stay within the [0; 360) range.
+  /// </remarks>
+  public Vector3 euler {
+    get { return _euler; }
+    set {
+      _euler = value;
+      NormlizeAngles();
+      rot = Quaternion.Euler(_euler.x, _euler.y, _euler.z);
+    }
+  }
+  Vector3 _euler;
 
-  /// <summary>Orientation of the transform.</summary>
-  public Quaternion rot;
+  /// <value>Orientation of the transform.</value>
+  public Quaternion rot { get; private set; }
+
+  /// <summary>Constructs a default instance.</summary>
+  /// <remarks>Required for the persistence to work correctly.</remarks>
+  /// <para>
+  /// By default position is <c>(0,0,0)</c>, Euler angles are <c>(0,0,0)</c>, and the rotation is
+  /// <c>Quaternion.identity</c>.  
+  /// </para>
+  /// <seealso href="https://docs.unity3d.com/ScriptReference/Vector3.html">
+  /// Unity3D: Vector3</seealso>
+  /// <seealso href="https://docs.unity3d.com/ScriptReference/Quaternion-identity.html">
+  /// Unity3D: Quaternion</seealso>
+  public PosAndRot() {
+  }
+
+  /// <summary>Constructs a copy of an object of the same type.</summary>
+  /// <param name="from">Source object.</param>
+  public PosAndRot(PosAndRot from) {
+    pos = from.pos;
+    euler = from.euler;
+  }
+
+  /// <summary>Constructs an object from a transform properties.</summary>
+  /// <param name="pos">Position of the transform.</param>
+  /// <param name="euler">Euler rotation of the transform.</param>
+  /// <seealso href="https://docs.unity3d.com/ScriptReference/Vector3.html">
+  /// Unity3D: Vector3</seealso>
+  /// <seealso href="https://docs.unity3d.com/ScriptReference/Transform.html">
+  /// Unity3D: Transform</seealso>
+  public PosAndRot(Vector3 pos, Vector3 euler) {
+    this.pos = pos;
+    this.euler = euler;
+  }
+
+  /// <summary>Gives a deep copy of the object.</summary>
+  /// <returns>New object.</returns>
+  public PosAndRot Clone() {
+    return new PosAndRot(this);
+  }
 
   /// <inheritdoc/>
   public string SerializeToString() {
-    var dir = rot * Vector3.forward;
-    var upwards = rot * Vector3.up;
-    if (Mathf.Approximately(Vector3.up.x, upwards.x)
-        && Mathf.Approximately(Vector3.up.y, upwards.y)
-        && Mathf.Approximately(Vector3.up.z, upwards.z)) {
-      return string.Format("{0},{1},{2}, {3},{4},{5}", pos.x, pos.y, pos.z, dir.x, dir.y, dir.z);
-    }
-    return string.Format("{0},{1},{2}, {3},{4},{5}, {6},{7},{8}",
-                         pos.x, pos.y, pos.z, dir.x, dir.y, dir.z, upwards.x, upwards.y, upwards.z);
+    return string.Format(
+        "{0},{1},{2}, {3},{4},{5}", pos.x, pos.y, pos.z, euler.x, euler.y, euler.z);
   }
 
   /// <inheritdoc/>
   public void ParseFromString(string value) {
     var elements = value.Split(',');
-    if (elements.Length != 6 && elements.Length != 9) {
+    if (elements.Length != 6) {
       throw new ArgumentException(
-          "PosAndRot type needs exactly 6 or 9 elements separated by a comma");
+          "PosAndRot type needs exactly 6 elements separated by a comma");
     }
-    var args = elements.Select(x => float.Parse(x)).ToArray();
-    var upwards = elements.Length == 9 ? new Vector3(args[6], args[7], args[8]) : Vector3.up;
+    var args = elements.Select(float.Parse).ToArray();
     pos = new Vector3(args[0], args[1], args[2]);
-    rot = Quaternion.LookRotation(new Vector3(args[3], args[4], args[5]), upwards);
+    euler = new Vector3(args[3], args[4], args[5]);
   }
 
   /// <inheritdoc/>
   public override string ToString() {
-    return string.Format("[PosAndRot Pos={0}, Dir={1}, Up={2}]",
-                         DbgFormatter.Vector(pos),
-                         DbgFormatter.Vector(rot * Vector3.forward),
-                         DbgFormatter.Vector(rot * Vector3.up));
+    return string.Format(
+        "[PosAndRot Pos={0}, Euler={1}]", DbgFormatter.Vector(pos), DbgFormatter.Vector(euler));
+  }
+  
+  /// <summary>
+  /// Ensures that all the angles are in the range of <c>[0; 360)</c>. 
+  /// </summary>
+  void NormlizeAngles() {
+    while (_euler.x > 360) _euler.x -= 360;
+    while (_euler.x < 0) _euler.x += 360;
+    while (_euler.y > 360) _euler.y -= 360;
+    while (_euler.y < 0) _euler.y += 360;
+    while (_euler.z > 360) _euler.z -= 360;
+    while (_euler.z < 0) _euler.z += 360;
   }
 }
 
