3140741aff0ef6bc36498075230ddf5e44388aac
ihsoft <igor.zavoychinskiy@gmail.com>
2017-05-31 20:46:12 -0700

    Minor comments change

--- Sources/Utils/Extensions/Dictionaries.cs
+++ Sources/Utils/Extensions/Dictionaries.cs
@@ -7,26 +7,26 @@ using System.Collections.Generic;
 
 namespace KSPDev.Extensions {
 
-/// <summary>Helper extensions for generic dictionary container.</summary>
+/// <summary>Helper extensions for the generic dictionary container.</summary>
 public static class Dictionaries {
   /// <summary>
-  /// Returns a value from dictionary by the key. If key is not defined yet then a new default entry
-  /// is created and returned.
+  /// Returns a value from the dictionary by a key. If the key is not present yet, then a new
+  /// default entry is created and returned.
   /// </summary>
   /// <example>
-  /// It's most useful when dealing with dictionaries of a complex type:
+  /// It's most useful when dealing with the dictionaries of a complex type:
   /// <code><![CDATA[
   /// var a = new Dictionary<int, HashSet<string>>();
-  /// // An empty string set for key 1 is created, and "abc" is added in it.
+  /// // An empty "string set" is created for the key 1, and "abc" is added into it.
   /// a.SetDefault(1).Add("abc");
-  /// // "def" is added into existing string set at key 1. 
+  /// // "def" is added into the existing string set at the key 1. 
   /// a.SetDefault(1).Add("def");
   /// ]]></code>
   /// </example>
-  /// <param name="dict">Dictionary to get value from.</param>
-  /// <param name="key">Key to lookup.</param>
-  /// <typeparam name="K">Type of the dictionary key.</typeparam>
-  /// <typeparam name="V">Type of the dictionary value.</typeparam>
+  /// <param name="dict">The dictionary to get value from.</param>
+  /// <param name="key">The key to lookup.</param>
+  /// <typeparam name="K">The type of the dictionary key.</typeparam>
+  /// <typeparam name="V">The type of the dictionary value.</typeparam>
   /// <returns>Either an existing value for the key or a default instance of the value.</returns>
   public static V SetDefault<K, V>(this Dictionary<K, V> dict, K key) where V : new() {
     V value;
