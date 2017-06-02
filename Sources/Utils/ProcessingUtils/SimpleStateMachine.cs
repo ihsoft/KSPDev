5bf2907b9ceacdd2820d04fda5f14e4b16e03ba5
ihsoft <igor.zavoychinskiy@gmail.com>
2017-06-02 02:22:33 -0700

    Major update in the state machine
    - Deprecate start/stop
    - Allow "null" state.
    - Add after change state notifier.

--- Sources/Utils/ProcessingUtils/SimpleStateMachine.cs
+++ Sources/Utils/ProcessingUtils/SimpleStateMachine.cs
@@ -20,190 +20,78 @@ namespace KSPDev.ProcessingUtils {
 /// conditions.
 /// </remarks>
 /// <typeparam name="T">The enum to use as the state constants.</typeparam>
-/// <example>
-/// Let's pretend there is a module with three states:
-/// <list type="bullet">
-/// <item>The state <c>One</c> can be transitioned into both <c>Two</c> and <c>Three</c>.</item>
-/// <item>The states <c>Two</c> and <c>Three</c> can only return back to <c>One</c>.</item>
-/// <item>In states <c>Two</c> and <c>Three</c> different menu options are available.</item>
-/// <item>In state <c>One</c> no menu options are available.</item>
-/// </list>
-/// <code><![CDATA[
-/// class MyModule : PartModule {
-///   enum MyState {
-///     One, Two, Three
-///   }
-///
-///   [KSPField(isPersistant = true)]
-///   public MyState persistedState = MyState.One;  // ALWAYS provide a default value!
-///
-///   SimpleStateMachine<MyState> linkStateMachine;
-///
-///   [KSPEvent(guiName = "State: TWO")]
-///   public void StateTwoMenuAction() {
-///     Debug.LogInfo("StateTwoMenuAction()");
-///   }
-///
-///   [KSPEvent(guiName = "State: THREE")]
-///   public void StateThreeMenuAction() {
-///     Debug.LogInfo("StateThreeMenuAction()");
-///   }
-///
-///   public override OnAwake() {
-///     linkStateMachine = new SimpleStateMachine<MyState>(true /* strict */);
-///     linkStateMachine.SetTransitionConstraint(
-///         MyState.One,
-///         new[] {MyState.Two, MyState.Three});
-///     linkStateMachine.SetTransitionConstraint(
-///         MyState.Two,
-///         new[] {MyState.One});
-///     linkStateMachine.SetTransitionConstraint(
-///         MyState.Three,
-///         new[] {MyState.One});
-///     linkStateMachine.AddStateHandlers(
-///         MyState.One,
-///         enterHandler: x => {
-///           Events["StateTwoMenuAction"].active = false;
-///           Events["StateThreeMenuAction"].active = false;
-///         });
-///     linkStateMachine.AddStateHandlers(
-///         MyState.Two,
-///         enterHandler: x => {
-///           Events["StateTwoMenuAction"].active = true;
-///           Events["StateThreeMenuAction"].active = false;
-///         });
-///     linkStateMachine.AddStateHandlers(
-///         MyState.Three,
-///         enterHandler: x => {
-///           Events["StateTwoMenuAction"].active = false;
-///           Events["StateThreeMenuAction"].active = true;
-///         });
-///   }
-///
-///   public override void OnStart(PartModule.StartState state) {
-///     linkStateMachine.Start(persistedState);  // Restore state from the save file.
-///   }
-///
-///   void OnDestory() {
-///     // Usually, this isn't needed. But if code needs to do a cleanup job it makes sense to wrap
-///     // it into a handler, and stop the machine in Unity destructor.
-///     linkStateMachine.Stop();
-///   }
-///
-///   public override OnUpdate() {
-///     if (Input.GetKeyDown("1")) {
-///       // This transition will always succceed. 
-///       stateMachine.currentState = MyState.One;
-///     }
-///     if (Input.GetKeyDown("2")) {
-///       // This transition will only succceed if current state is MyState.One. 
-///       stateMachine.currentState = MyState.Two;
-///     }
-///     if (Input.GetKeyDown("3")) {
-///       // This transition will only succceed if current state is MyState.One. 
-///       stateMachine.currentState = MyState.Three;
-///     }
-///   }
-/// }
-/// ]]></code>
-/// <para>
-/// The same logic could be achivied in a different way. Instead of enabling/disabling all the menu
-/// items in every "enter" handler the code could define "leave" handlers that would disable the
-/// related menu item. This way every state handler would control its own menu item without
-/// interacting with any existing or the future items.
-/// </para>
-/// </example>
+/// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachine1"/></example>
 public sealed class SimpleStateMachine<T> where T : struct, IConvertible {
   /// <summary>Current state of the machine.</summary>
   /// <remarks>
+  /// <para>
   /// Setting the same state as the current one is a NO-OP. Setting of a new state may throw an
   /// exception in the strict mode.
+  /// </para>
+  /// <para>
+  /// The initial state is always <c>null</c>, whiсh means <i>STOPPED</i>. The caller must set the
+  /// initial state before starting using the machine. In spite of the other transitions, the
+  /// initial state change is not restricted by the state transition constraints, regardless to the
+  /// <see cref="isStrict"/> setting.
+  /// </para>
+  /// <para>
+  /// To <i>STOP</i> the machine, just set the current state to <c>null</c>. No "enter state"
+  /// handlers will be executed in this case, but all the "leave state" handlers will do their job.
+  /// </para>
   /// </remarks>
-  /// <value>The current state.</value>
+  /// <value>The current state. It can be <c>null</c>.</value>
   /// <seealso cref="isStrict"/>
-  /// <seealso cref="ForceSetState"/>
-  public T currentState {
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineFree"/></example>
+  public T? currentState {
     get { return _currentState; }
     set { SetState(value); }
   }
-  T _currentState;
-
-  /// <summary>Tells if the state machine is started.</summary>
-  /// <value>The started state.</value>
-  /// <seealso cref="Start"/>
-  public bool isStarted { get; private set; }
+  T? _currentState;
 
   /// <summary>Tells if all the transitions must be excplicitly declared.</summary>
-  /// <remarks>
-  /// The state transitions are defined via <see cref="SetTransitionConstraint"/>.
-  /// </remarks>
   /// <value>The strict mode state.</value>
   /// <seealso cref="SetTransitionConstraint"/>
-  /// <seealso cref="ResetTransitionConstraint"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineStrict"/></example>
   public bool isStrict { get; private set; }
 
   /// <summary>Delegate for a callback which notifies about a state change.</summary>
-  /// <param name="state">The current state of the machine.</param>
-  public delegate void OnChange(T state);
+  /// <param name="state">
+  /// The state of the machine. Its exact meaning depends on the circumstances under which the
+  /// callback has been called.
+  /// </param>
+  /// <seealso cref="currentState"/>
+  /// <seealso cref="AddStateHandlers"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineFree"/></example>
+  public delegate void OnChange(T? state);
 
-  /// <summary>Special debug delegate to track the state changes.</summary>
-  /// <remarks>This callback is called before the actual state change.</remarks>
+  /// <summary>Delegate to track an arbitrary state transition.</summary>
   /// <param name="fromState">The state before the change.</param>
   /// <param name="toState">The state after the change.</param>
-  public delegate void OnDebugChange(T fromState, T toState);
+  /// <seealso cref="currentState"/>
+  /// <seealso cref="onAfterTransition"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineFree"/></example>
+  public delegate void OnStateChangeHandler(T? fromState, T? toState);
 
-  /// <summary>
-  /// Debug handler to track the state changes. Avoid using it in the normal code logic.
-  /// </summary>
-  public OnDebugChange OnDebugStateChange;
+  /// <summary>Event that fires when the state machine has changed its state.</summary>
+  /// <remarks>The event is fired <i>after</i> the actual state change.</remarks>
+  /// <seealso cref="currentState"/>
+  /// <seealso cref="OnStateChangeHandler"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineFree"/></example>
+  public event OnStateChangeHandler onAfterTransition;
 
   readonly Dictionary<T, HashSet<OnChange>> enterHandlers = new Dictionary<T, HashSet<OnChange>>();
   readonly Dictionary<T, HashSet<OnChange>> leaveHandlers = new Dictionary<T, HashSet<OnChange>>();
   readonly Dictionary<T, T[]> transitionContstraints = new Dictionary<T, T[]>();
 
-  /// <summary>Constructs a new unstarted state machine.</summary>
+  /// <summary>Constructs a new uninitialized state machine.</summary>
   /// <param name="strict">The strict mode.</param>
   /// <seealso cref="isStrict"/>
+  /// <seealso cref="currentState"/>
+  /// <seealso cref="SetTransitionConstraint"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineStrict"/></example>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineFree"/></example>
   public SimpleStateMachine(bool strict) {
     isStrict = strict;
-  }
-
-  /// <summary>Starts the state machine and makes it available for the state transitions.</summary>
-  /// <remarks>
-  /// <para>
-  /// Until the machine is started, the state transitions are not possible. An attempt to move the
-  /// machine into any state will result in a <see cref="InvalidOperationException"/> exception.
-  /// </para>
-  /// <para>Starting of the machine will trigger an enter state event.</para>
-  /// </remarks>
-  /// <param name="startState">The initial state of the machine.</param>
-  /// <seealso cref="isStarted"/>
-  /// <seealso cref="AddStateHandlers"/>
-  public void Start(T startState) {
-    CheckIsNotStarted();
-    isStarted = true;
-    _currentState = startState;
-    if (OnDebugStateChange != null) {
-      OnDebugStateChange(_currentState, _currentState);
-    }
-    FireEnterState();
-  }
-
-  /// <summary>Stops the state machine making it unavailable for any state transition.</summary>
-  /// <remarks>
-  /// If the machine is not started then this call is a NO-OP. Stoping of the started machine will
-  /// trigger a leave state event.
-  /// </remarks>
-  /// <seealso cref="isStarted"/>
-  /// <seealso cref="AddStateHandlers"/>
-  public void Stop() {
-    if (isStarted) {
-      if (OnDebugStateChange != null) {
-        OnDebugStateChange(_currentState, _currentState);
-      }
-      FireLeaveState();
-      isStarted = false;
-    }
   }
 
   /// <summary>Defines a state and the allowed target states for it.</summary>
@@ -213,6 +101,8 @@ public sealed class SimpleStateMachine<T> where T : struct, IConvertible {
   /// <param name="fromState">The source state.</param>
   /// <param name="toStates">The list of the states that are allowed as the targets.</param>
   /// <seealso cref="isStrict"/>
+  /// <seealso cref="currentState"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineStrict"/></example>
   public void SetTransitionConstraint(T fromState, T[] toStates) {
     CheckIsNotStarted();
     transitionContstraints.Remove(fromState);
@@ -221,47 +111,36 @@ public sealed class SimpleStateMachine<T> where T : struct, IConvertible {
 
   /// <summary>Clears the transitions for the source state if any.</summary>
   /// <param name="fromState">The source state to clear the tarnsitions for.</param>
-  /// <seealso cref="isStrict"/>
+  /// <seealso cref="SetTransitionConstraint"/>
   public void ResetTransitionConstraint(T fromState) {
     CheckIsNotStarted();
     transitionContstraints.Remove(fromState);
   }
 
-  /// <summary>Changes the current state bypassing all the transition checks.</summary>
-  /// <remarks>
-  /// It's discouraged to use this method in the normal flow. However, it may be handy when
-  /// recovering a module from an unknown state (e.g. an unexpected exception was thrown in the
-  /// middle of the process).
-  /// </remarks>
-  /// <param name="newState">
-  /// The new state. It can be a state that is not mentioned in any state transition.
-  /// </param>
-  public void ForceSetState(T newState) {
-    CheckIsStarted();
-    if (OnDebugStateChange != null) {
-      OnDebugStateChange(_currentState, newState);
-    }
-    if (isStarted) {
-      FireLeaveState();
-    }
-    _currentState = newState;
-    FireEnterState();
-  }
-
   /// <summary>Adds a state change event.</summary>
   /// <remarks>
+  /// <para>
+  /// When the state is changed by setting the <see cref="currentState"/> property, the transition
+  /// callbacks are only called when the state has actually changed.
+  /// </para>
+  /// <para>
   /// Note, that the code must not expect that the handlers will be called in the same order as they
   /// were added. Each handler must be independent from the others.
+  /// </para>
   /// </remarks>
   /// <param name="state">The state to call a callback on.</param>
   /// <param name="enterHandler">
   /// The callback to call when the state machine has switched to a new state. The callback is
-  /// triggered <i>after</i> the state has actually changed.
+  /// triggered <i>after</i> the state has actually changed. The callback's parameter is the
+  /// <i>old</i> state, from which the machine has switched.
   /// </param>
   /// <param name="leaveHandler">
   /// The callback to call when the state machine is going to leave the current state. The callback
-  /// is triggered <i>before</i> the state has actually changed. 
+  /// is triggered <i>before</i> the state has actually changed. The callback's parameter is the
+  /// <i>new</i> state, to which the machine is going to switch. 
   /// </param>
+  /// <seealso cref="currentState"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineFree"/></example>
   public void AddStateHandlers(
       T state, OnChange enterHandler = null, OnChange leaveHandler = null) {
     CheckIsNotStarted();
@@ -293,17 +172,18 @@ public sealed class SimpleStateMachine<T> where T : struct, IConvertible {
   /// <returns><c>true</c> if the transition is allowed.</returns>
   /// <seealso cref="isStrict"/>
   /// <seealso cref="SetTransitionConstraint"/>
+  /// <example><code source="Examples/ProcessingUtils/SimpleStateMachine-Examples.cs" region="SimpleStateMachineStrict"/></example>
   public bool CheckCanSwitchTo(T newState) {
     return !isStrict
-        || transitionContstraints.ContainsKey(_currentState)
-           && transitionContstraints[_currentState].IndexOf(newState) != -1;
+        || transitionContstraints.ContainsKey(_currentState.Value)
+           && transitionContstraints[_currentState.Value].IndexOf(newState) != -1;
   }
 
   #region Local utility methods
   /// <summary>Verifies that the state machine is started.</summary>
   /// <exception cref="InvalidOperationException">If state machine is not yet started.</exception>
   void CheckIsStarted() {
-    if (!isStarted) {
+    if (!currentState.HasValue) {
       throw new InvalidOperationException("Not allowed in STOPPED state");
     }
   }
@@ -311,7 +191,7 @@ public sealed class SimpleStateMachine<T> where T : struct, IConvertible {
   /// <summary>Verifies that the state machine is <i>not</i> started.</summary>
   /// <exception cref="InvalidOperationException">If state machine is already started.</exception>
   void CheckIsNotStarted() {
-    if (isStarted) {
+    if (currentState.HasValue) {
       throw new InvalidOperationException("Not allowed in STARTED state");
     }
   }
@@ -320,36 +200,51 @@ public sealed class SimpleStateMachine<T> where T : struct, IConvertible {
   /// Changes the machine's state if the current and the new states are different. Checks if the
   /// transition is allowed before actually changing the state.
   /// </summary>
-  /// <param name="newState">The state to change to.</param>
+  /// <param name="newState">
+  /// The state to change to. If <c>null</c> then the machine will be stopped.
+  /// </param>
   /// <seealso cref="CheckCanSwitchTo"/>
   /// <exception cref="InvalidOperationException">If the transition is not allowed.</exception>
-  void SetState(T newState) {
-    CheckIsStarted();
+  void SetState(T? newState) {
     if (!_currentState.Equals(newState)) {
-      if (!CheckCanSwitchTo(newState)) {
-        throw new InvalidOperationException(string.Format(
-            "Transition {0}=>{1} is not allowed", _currentState, newState));
+      var oldState = _currentState;
+      if (oldState.HasValue && newState.HasValue) {
+        if (!CheckCanSwitchTo(newState.Value)) {
+          throw new InvalidOperationException(string.Format(
+              "Transition {0}=>{1} is not allowed", oldState.Value, newState.Value));
+        }
       }
-      ForceSetState(newState);
+      if (oldState.HasValue) {
+        FireLeaveState(newState);
+      }
+      _currentState = newState;
+      if (newState.HasValue) {
+        FireEnterState(oldState);
+      }
+      if (onAfterTransition != null) {
+        onAfterTransition(oldState, newState);
+      }
     }
   }
 
   /// <summary>Notifies all the handlers about leaving the current state.</summary>
-  void FireLeaveState() {
+  /// <param name="newState">The new state where the machine is going to.</param>
+  void FireLeaveState(T? newState) {
     HashSet<OnChange> handlers;
-    if (leaveHandlers.TryGetValue(_currentState, out handlers)) {
+    if (leaveHandlers.TryGetValue(_currentState.Value, out handlers)) {
       foreach (var @event in handlers) {
-        @event.Invoke(_currentState);
+        @event.Invoke(newState);
       }
     }
   }
 
   /// <summary>Notifies all the handlers about entering a new state.</summary>
-  void FireEnterState() {
+  /// <param name="oldState">The old state where the machine is going from.</param>
+  void FireEnterState(T? oldState) {
     HashSet<OnChange> handlers;
-    if (enterHandlers.TryGetValue(_currentState, out handlers)) {
+    if (enterHandlers.TryGetValue(_currentState.Value, out handlers)) {
       foreach (var @event in handlers) {
-        @event.Invoke(_currentState);
+        @event.Invoke(oldState);
       }
     }
   }
---
+++ Sources/Utils/docs_project/Examples/ProcessingUtils/SimpleStateMachine-Examples.cs
@@ -0,0 +1,187 @@
+﻿// Kerbal Development tools - Examples
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using KSPDev.ProcessingUtils;
+using KSPDev.LogUtils;
+using System;
+using UnityEngine;
+
+namespace Examples {
+
+#region SimpleStateMachine1
+/// <summary>A class that demonstrates a simple state module with three states.</summary>
+/// <remarks>
+/// There are the follwoing rules for the state changes:
+/// <list type="bullet">
+/// <item>The state <c>One</c> can be transitioned into both <c>Two</c> and <c>Three</c>.</item>
+/// <item>The states <c>Two</c> and <c>Three</c> can only return back to <c>One</c>.</item>
+/// <item>In states <c>Two</c> and <c>Three</c> different menu options are available.</item>
+/// <item>In state <c>One</c> no menu options are available.</item>
+/// </list>
+/// </remarks>
+class SimpleStateMachine1 : PartModule {
+  public enum State {
+    One,
+    Two,
+    Three
+  }
+
+  [KSPField(isPersistant = true)]
+  public State persistedState = State.One;  // ALWAYS provide a default value!
+
+  SimpleStateMachine<State> stateMachine;
+
+  [KSPEvent(guiName = "State: TWO")]
+  public void StateTwoMenuAction() {
+    Debug.Log("StateTwoMenuAction()");
+  }
+
+  [KSPEvent(guiName = "State: THREE")]
+  public void StateThreeMenuAction() {
+    Debug.Log("StateThreeMenuAction()");
+  }
+
+  public override void OnAwake() {
+    stateMachine = new SimpleStateMachine<State>(strict: true);
+    // State ONE can be transitioned into both TWO and THREE.
+    stateMachine.SetTransitionConstraint(
+        State.One,
+        new[] {State.Two, State.Three});
+    // State TWO can only get back to ONE.
+    stateMachine.SetTransitionConstraint(
+        State.Two,
+        new[] {State.One});
+    // State THREE can only get back to ONE.
+    stateMachine.SetTransitionConstraint(
+        State.Three,
+        new[] {State.One});
+    // No menus available in state ONE.
+    stateMachine.AddStateHandlers(
+        State.One,
+        enterHandler: oldState => {
+          Events["StateTwoMenuAction"].active = false;
+          Events["StateThreeMenuAction"].active = false;
+        },
+        leaveHandler: newState => Debug.LogFormat("Move from ONE to {0}", newState));
+    // Only TWO-menu is available in the state TWO.
+    stateMachine.AddStateHandlers(
+        State.Two,
+        enterHandler: oldState => {
+          Events["StateTwoMenuAction"].active = true;
+          Events["StateThreeMenuAction"].active = false;
+        });
+    // Only THREE-menu is available in the state THREE.
+    stateMachine.AddStateHandlers(
+        State.Three,
+        enterHandler: oldState => {
+          Events["StateTwoMenuAction"].active = false;
+          Events["StateThreeMenuAction"].active = true;
+        });
+  }
+
+  public override void OnStart(PartModule.StartState state) {
+    stateMachine.currentState = persistedState;  // Restore state from the save file.
+  }
+
+  void OnDestory() {
+    // Usually, this isn't needed. But if code needs to do a cleanup job it makes sense to wrap
+    // it into a handler, and stop the machine in the Unity destructor.
+    stateMachine.currentState = null;
+  }
+
+  public override void OnUpdate() {
+    if (Input.GetKeyDown("1")) {
+      // This transition will always succceed. 
+      stateMachine.currentState = State.One;
+    }
+    if (Input.GetKeyDown("2")) {
+      // This transition will only succceed if current state is MyState.One. 
+      stateMachine.currentState = State.Two;
+    }
+    if (Input.GetKeyDown("3")) {
+      // This transition will only succceed if current state is MyState.One. 
+      stateMachine.currentState = State.Three;
+    }
+  }
+}
+#endregion
+
+#region SimpleStateMachineStrict
+class SimpleStateMachineStrict {
+  public enum State {
+    One,
+    Two,
+    Three
+  };
+
+  public void TestMachine() {
+    var sm = new SimpleStateMachine<State>(strict: true);
+    sm.SetTransitionConstraint(State.One, new State[] { State.Two });
+    sm.AddStateHandlers(
+        State.One,
+        enterHandler: newState => LogFromTo(sm.currentState, newState),
+        leaveHandler: newState => LogFromTo(sm.currentState, newState));
+
+    sm.currentState = State.One;  // Start the machine.
+    // Logs: Move from NULL to One
+
+    if (sm.CheckCanSwitchTo(State.Two)) {
+      sm.currentState = State.Two;  // An allowed tranistion.
+      // Logs: Move from One to Two
+    }
+
+    if (sm.CheckCanSwitchTo(State.Three)) {
+      // This will never happen.
+      sm.currentState = State.Three;
+    }
+
+    sm.currentState = null;  // Stop the machine.
+    // Logs: Move Two to NULL
+  }
+
+  void LogFromTo(State? from, State? to) {
+    Debug.LogFormat(
+        "Move from {0} to {1}", DbgFormatter.Nullable(from), DbgFormatter.Nullable(to));
+  }
+}
+#endregion
+
+#region SimpleStateMachineFree
+class SimpleStateMachineFree {
+  public enum State {
+    One,
+    Two,
+    Three
+  };
+
+  public void TestMachine() {
+    var sm = new SimpleStateMachine<State>(strict: false);
+    sm.AddStateHandlers(
+        State.One,
+        enterHandler: oldState => Debug.Log("Now in state ONE"),
+        leaveHandler: newState => Debug.LogFormat("Going into state: {0}", newState));
+    sm.onAfterTransition += (from, to) => Debug.LogFormat(
+        "Move from {0} to {1}", DbgFormatter.Nullable(from), DbgFormatter.Nullable(to));
+
+    sm.currentState = State.One;  // Start the machine.
+    // Logs:
+    // Now in state ONE
+    // Move from NULL to One
+
+    sm.currentState = State.Two;
+    // Logs:
+    // Going into state: Two
+    // Move from One to Two
+
+    sm.currentState = State.Three;
+    // Logs: Move from Two to Three
+
+    sm.currentState = null;  // Stop the machine.
+    // Logs: Move Three to NULL
+  }
+}
+#endregion
+
+}  // namespace
+
