598a39080d749cfae547b5158a3b918224008639
ihsoft <igor.zavoychinskiy@gmail.com>
2017-06-15 02:28:03 -0700

    Minor docs change

--- Sources/Utils/ConfigUtils/AbstractPersistentFieldAttribute.cs
+++ Sources/Utils/ConfigUtils/AbstractPersistentFieldAttribute.cs
@@ -7,8 +7,8 @@ using System;
 namespace KSPDev.ConfigUtils {
   
 /// <summary>A base for any persitent field annotation.</summary>
-/// <remarks>Descendands must initialize at least <see cref="_ordinaryTypeProto"/> field. If
-/// <see cref="_collectionTypeProto"/> is set then the field is considered a persistent
+/// <remarks>Descendands must initialize at least <see cref="ordinaryTypeProto"/> field. If
+/// <see cref="collectionTypeProto"/> is set then the field is considered a persistent
 /// collection of values.
 /// <para>See more examples in <see cref="PersistentFieldsFileAttribute"/>.</para>
 /// </remarks>
@@ -37,25 +37,30 @@ public abstract class AbstractPersistentFieldAttribute : Attribute {
   public readonly string[] path;
 
   /// <summary>A tag for a group of fields.</summary>
-  /// <remarks>Group can be used when reading/writing values via <see cref="ConfigAccessor"/>
-  /// to process only a subset of the persistent fields of the class. It's case-insensitive.
+  /// <remarks>
+  /// Group can be used when reading/writing values via <see cref="ConfigAccessor"/> to process only
+  /// a subset of the persistent fields of the class. It's case-insensitive.
   /// </remarks>
   public string group = "";
 
   /// <summary>A proto that (de)serializes field's value as a simple string.</summary>
-  /// <remarks>This type must be a descendant of <see cref="AbstractOrdinaryValueTypeProto"/>.
+  /// <remarks>
+  /// This type must be a descendant of <see cref="AbstractOrdinaryValueTypeProto"/>.
   /// </remarks>
   protected Type _ordinaryTypeProto;
 
   /// <summary>A proto that handles field's value as a collection of persistent values.</summary>
   /// <remarks>If it's <c>null</c> then field is assumed to be not a collection.</remarks>
-  /// <para>This type must be a descendant of <see cref="AbstractCollectionTypeProto"/>.
+  /// <para>
+  /// This type must be a descendant of <see cref="AbstractCollectionTypeProto"/>.
   /// </para>
   protected Type _collectionTypeProto;
 
-  /// <param name="cfgPath">A path to the fields's value in the config. Components must be separated
-  /// by symbol '/'. The path is relative, the absolute path is determined when doing actual
-  /// (de)serialization. The path is case-insensitive.</param>
+  /// <param name="cfgPath">
+  /// A path to the fields's value in the config. Components must be separated by symbol <c>/</c>.
+  /// The path is relative, the absolute path is determined when doing actual (de)serialization.
+  /// The path is case-insensitive.
+  /// </param>
   protected AbstractPersistentFieldAttribute(string cfgPath) {
     this.path = cfgPath.Split('/');
   }
