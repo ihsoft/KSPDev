579f88aec9eac99401a8fc8f6a5dffdfe1a10ba8
ihsoft <igor.zavoychinskiy@gmail.com>
2017-06-15 20:17:58 -0700

    Add LocalizationLoader

---
+++ Sources/Utils/docs_project/Examples/GUIUtils/LocalizationLoader-Examples.cs
@@ -0,0 +1,26 @@
+﻿// Kerbal Development tools - Examples
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using KSPDev.GUIUtils;
+
+namespace Examples {
+
+#region LocalizationLoaderDemo1
+public class LocalizationLoaderDemo1 : PartModule {
+  [KSPField(guiName = "just-in-case text", guiActive = true)]
+  [LocalizableItem(
+      tag = "#tag1",
+      defaultTemplate = "English text 1",
+      description = "A field which demonstrates a localizable GUI string")]
+  public string field1 = "";
+
+  public override void OnAwake() {
+    base.OnAwake();
+    // The loader will overwrite guiName in field1.
+    LocalizationLoader.LoadItemsInModule(this);
+  }
+}
+#endregion
+
+}  // namespace
