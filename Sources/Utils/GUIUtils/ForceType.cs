e8d911a6a024549d076e8284d801909fd092765a
ihsoft <igor.zavoychinskiy@gmail.com>
2017-06-20 21:01:52 -0700

    Add ForceType class

---
+++ Sources/Utils/GUIUtils/ForceType.cs
@@ -0,0 +1,105 @@
+﻿// Kerbal Development tools.
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+namespace KSPDev.GUIUtils {
+
+/// <summary>
+/// Localized message formatting class for a numeric value that represents a <i>force</i>. The
+/// resulted message may have a unit specification.
+/// </summary>
+/// <remarks>
+/// <para>
+/// Use it as a generic parameter when creating a <see cref="LocalizableMessage"/> descendants.
+/// </para>
+/// <para>
+/// The class uses the unit name localizations from the stock module <c>ModuleEnviroSensor</c>. In
+/// case of this module is deprecated or the tags are changed, the default English values will be
+/// used for the unit names.
+/// </para>
+/// </remarks>
+/// <include file="SpecialDocTags.xml" path="Tags/MessageTypeWithArg/*"/>
+/// <include file="SpecialDocTags.xml" path="Tags/MessageArgumentType/*"/>
+/// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo1"/></example>
+/// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo2_FormatDefault"/></example>
+/// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo2_FormatFixed"/></example>
+public sealed class ForceType {
+  /// <summary>Localized suffix for the "kilonewton" untis. Scale x1.</summary>
+  public static readonly Message kiloNewton = new Message(
+      "#autoLOC_7001408", defaultTemplate: " kN",
+      description: "Kilonewton unit for a force value");
+
+  /// <summary>A wrapped numeric value.</summary>
+  /// <remarks>This is the original non-rounded and unscaled value.</remarks>
+  public readonly double value;
+
+  /// <summary>Constructs an object from a numeric value.</summary>
+  /// <param name="value">The numeric value in kilonewtons.</param>
+  /// <seealso cref="Format"/>
+  /// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo1"/></example>
+  /// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo2_FormatDefault"/></example>
+  /// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo2_FormatFixed"/></example>
+  public ForceType(double value) {
+    this.value = value;
+  }
+
+  /// <summary>Coverts a numeric value into a type object.</summary>
+  /// <param name="value">The numeric value to convert.</param>
+  /// <returns>An object.</returns>
+  public static implicit operator ForceType(double value) {
+    return new ForceType(value);
+  }
+
+  /// <summary>Converts a type object into a numeric value.</summary>
+  /// <param name="obj">The object type to convert.</param>
+  /// <returns>A numeric value.</returns>
+  public static implicit operator double(ForceType obj) {
+    return obj.value;
+  }
+
+  /// <summary>Formats the value into a human friendly string with a unit specification.</summary>
+  /// <remarks>
+  /// <para>
+  /// The method tries to keep the resulted string meaningful and as short as possible. For this
+  /// reason the big values may be scaled down and/or rounded.
+  /// </para>
+  /// <para>The base force unit in the game is <i>kilonewton</i>. I.e. value <c>1.0</c> in the game
+  /// units is <c>1 kN</c>. Keep it in mind when passing the argument.
+  /// </para>
+  /// </remarks>
+  /// <param name="value">The numeric value to format.</param>
+  /// <param name="format">
+  /// The specific float number format to use. If the format is not specified, then it's choosen
+  /// basing on the value.
+  /// </param>
+  /// <returns>A formatted and localized string</returns>
+  /// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo2_FormatDefault"/></example>
+  /// <example><code source="Examples/GUIUtils/ForceType-Examples.cs" region="ForceTypeDemo2_FormatFixed"/></example>
+  public static string Format(double value, string format = null) {
+    if (format != null) {
+      return value.ToString(format) + kiloNewton.Format();
+    }
+    if (value < 0.1) {
+      return value.ToString("0.00#") + kiloNewton.Format();
+    }
+    if (value < 1.0) {
+      return value.ToString("0.00") + kiloNewton.Format();
+    }
+    if (value < 10.0) {
+      return value.ToString("0.0#") + kiloNewton.Format();
+    }
+    if (value < 100.0) {
+      return value.ToString("0.#") + kiloNewton.Format();
+    }
+    return value.ToString("0") + kiloNewton.Format();
+  }
+
+  /// <summary>Returns a string formatted as a human friendly force specification.</summary>
+  /// <returns>A string representing the value.</returns>
+  /// <seealso cref="Format"/>
+  public override string ToString() {
+    return Format(value);
+  }
+}
+
+}  // namespace
---
+++ Sources/Utils/docs_project/Examples/GUIUtils/ForceType-Examples.cs
@@ -0,0 +1,59 @@
+﻿// Kerbal Development tools - Examples
+// Author: igor.zavoychinskiy@gmail.com
+// This software is distributed under Public domain license.
+
+using KSPDev.LogUtils;
+using KSPDev.GUIUtils;
+using UnityEngine;
+
+namespace Examples {
+
+#region ForceTypeDemo1
+public class ForceTypeDemo1 : PartModule {
+  // This message uses a force type as a parameter.
+  static readonly Message<ForceType> msg1 = new Message<ForceType>(
+      "#DistanceTypeDemo_msg1", defaultTemplate: "Force is: <<1>>");
+
+  // Depending on the current language in the system, this method will present different unit names. 
+  void ShowForce() {
+    Debug.Log(msg1.Format(0.051));
+    // Prints: "Force is: 0.051 kN"
+    Debug.Log(msg1.Format(0.45));
+    // Prints: "Force is: 0.45 kN"
+    Debug.Log(msg1.Format(95.45));
+    // Prints: "Force is: 95.5 kN"
+    Debug.Log(msg1.Format(120.45));
+    // Prints: "Force is: 120 kN"
+    Debug.Log(msg1.Format(9535.45));
+    // Prints: "Force is: 9536 kN"
+  }
+}
+#endregion
+
+public class ForceTypeDemo2 {
+  void FormatDefault() {
+    #region ForceTypeDemo2_FormatDefault
+    Debug.Log(ForceType.Format(0.051));
+    // Prints: "Force is: 0.051 kN"
+    Debug.Log(ForceType.Format(0.45));
+    // Prints: "Force is: 0.45 kN"
+    Debug.Log(ForceType.Format(95.45));
+    // Prints: "Force is: 95.5 kN"
+    Debug.Log(ForceType.Format(120.45));
+    // Prints: "Force is: 120 kN"
+    Debug.Log(ForceType.Format(9535.45));
+    // Prints: "Force is: 9536 kN"
+    #endregion
+  }
+
+  void FormatFixed() {
+    #region ForceTypeDemo2_FormatFixed
+    Debug.Log(DistanceType.Format(1234.5678, format: "0.0000"));
+    // Prints: "Force is: 1234.5678 m"
+    Debug.Log(DistanceType.Format(1234.5678, format: "0.00"));
+    // Prints: "Force is: 1234.57 m"
+    #endregion
+  }
+}
+
+}  // namespace
