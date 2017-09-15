5b26068165f8bb66574aeebcac3a7c0c838ba97b
ihsoft <igor.zavoychinskiy@gmail.com>
2017-09-15 00:18:51 -0700

    Move the release builder into own repository
    https://github.com/ihsoft/KSPDev_ReleaseBuilder

--- Sources/ReleaseBuilder/template_make_binary.cmd
+++
@@ -1,8 +0,0 @@
-REM This is a template for a script to build mod's binary(-ies).
-REM Adjust path to the compiler and set the right name of the mod's project file.
-REM Ensure mod's project has release configuration setup.
-REM Verify the output path of the Release configuration, it must match the release project setup.
-
-@echo off
-REM Until new major version of C# is released the build number in the path will keep counting.
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild ..\Source\ProjectFileName.csproj /t:Rebuild /p:Configuration=Release
