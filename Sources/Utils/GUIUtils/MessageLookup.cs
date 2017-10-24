2a4f82dbdf87495d753008e25f01e291dc8dee51
ihsoft <igor.zavoychinskiy@gmail.com>
2017-10-23 20:26:07 -0700

    Drop noDefault in the message lookup

--- Sources/Utils/GUIUtils/MessageLookup.cs
+++ Sources/Utils/GUIUtils/MessageLookup.cs
@@ -26,50 +26,40 @@ public sealed class MessageLookup<T> where T : struct {
   public readonly Dictionary<T, Message> messages;
 
   /// <summary>Message to return when the requested key is not found.</summary>
-  /// <seealso cref="stockDefaultMessage"/>
   public readonly Message defaultMessage;
-
-  /// <summary>Default message to return when the requested key is not found.</summary>
-  /// <seealso cref="defaultMessage"/>
-  public readonly Message stockDefaultMessage = new Message(null, defaultTemplate: "#NONE#");
 
   /// <summary>Constructs a lookup from the provided dictionary.</summary>
   /// <remarks>
   /// It's encouraged to construct the dictionary from a statically defined messages instead of
-  /// creating them in place. The <c>LocalizationTool</c> cann only find the static messages.
+  /// creating them in place. The <c>LocalizationTool</c> can only find the static messages.
   /// </remarks>
   /// <param name="initDict">
   /// The dictionary to use as the lookup. If <c>null</c> when a new empty dictionary will be
   /// created for <see cref="messages"/>.
   /// </param>
   /// <param name="defaultMessage">
-  /// The message to return from the <see cref="Lookup"/> method when no key found. If omitted then
-  /// the non-localizable  <see cref="stockDefaultMessage"/> will be returned.
+  /// The message to return from the <see cref="Lookup"/> method when no key found.
   /// </param>
   /// <seealso cref="Lookup"/>
   /// <example><code source="Examples/GUIUtils/MessageLookup-Examples.cs" region="MessageLookupDemo_Simple"/></example>
   public MessageLookup(Dictionary<T, Message> initDict = null, Message defaultMessage = null) {
     messages = initDict ?? new Dictionary<T, Message>();
-    this.defaultMessage = defaultMessage ?? stockDefaultMessage;
+    this.defaultMessage = defaultMessage;
   }
 
-  /// <summary>Find and return a message for the provided key.</summary>
+  /// <summary>Finds and returns a message for the provided key.</summary>
   /// <param name="key">The key to find a message for.</param>
-  /// <param name="noDefault">
-  /// If <c>true</c> and no key was found, then <c>null</c> is returned instead of the
-  /// <see cref="defaultMessage"/>.
-  /// </param>
   /// <returns>
-  /// The relevant message if the <paramref name="key"/> is found. Otherwise, the
-  /// <see cref="defaultMessage"/> or <c>null</c>.
+  /// The relevant message if the <paramref name="key"/> is found. Otherwise, either the
+  /// <see cref="defaultMessage"/> or a string represenation of the key. It's never <c>null</c>. 
   /// </returns>
   /// <example><code source="Examples/GUIUtils/MessageLookup-Examples.cs" region="MessageLookupDemo_WithDefault"/></example>
   /// <example><code source="Examples/GUIUtils/MessageLookup-Examples.cs" region="MessageLookupDemo_WithStockDefault"/></example>
   /// <seealso cref="defaultMessage"/>
-  public Message Lookup(T key, bool noDefault = false) {
+  public Message Lookup(T key) {
     Message res;
     if (!messages.TryGetValue(key, out res)) {
-      return noDefault ? null : defaultMessage;
+      return defaultMessage ?? new Message(null, defaultTemplate: key.ToString());
     }
     return res;
   }
