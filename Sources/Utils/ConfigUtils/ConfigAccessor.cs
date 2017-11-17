42da5db3538d5a92154d858f512bc7be4ad3c3e9
ihsoft <igor.zavoychinskiy@gmail.com>
2017-11-16 22:07:12 -0800

    Add new public group - PartPersistant

--- Sources/Utils/ConfigUtils/ConfigAccessor.cs
+++ Sources/Utils/ConfigUtils/ConfigAccessor.cs
@@ -20,6 +20,16 @@ public static class StdPersistentGroups {
   /// from the default.
   /// </remarks>
   public const string Default = "";
+
+  /// <summary>
+  /// A public group that designates the fields that are saved/loaded durint the vessel persistense.
+  /// </summary>
+  /// <remarks>
+  /// The fields of this group are expected to be loaded/saved from the related <c>PartModule</c>
+  /// methods: <c>OnSave</c> and <c>OnLoad</c>. Dealing with them outside of this logic is highly
+  /// discouraged.
+  /// </remarks>
+  public const string PartPersistant = "PartPersistant";
 }
 
 /// <summary>A service class that simplifies accessing configuration files.</summary>
