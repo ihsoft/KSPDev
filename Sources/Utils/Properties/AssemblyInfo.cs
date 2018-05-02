949673d5ea6e00bc0c16b9e51befc8e0d883235d
ihsoft <igor.zavoychinskiy@gmail.com>
2018-05-01 22:24:11 -0700

    Release 0.32

--- Sources/Utils/Properties/AssemblyInfo.cs
+++ Sources/Utils/Properties/AssemblyInfo.cs
@@ -25,6 +25,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can use the default the Revision and 
 // Build Numbers by using the '*' as shown below:
 [assembly: AssemblyVersion ("0.32.*")]
-[assembly: AssemblyInformationalVersion ("0.32-pre for KSP v1.4")]
+[assembly: AssemblyInformationalVersion ("0.32 for KSP v1.4+")]
 [assembly: Guid ("89e3b337-b770-4422-b56e-159590c94c9d")]
 [assembly: AssemblyFlags (AssemblyNameFlags.PublicKey | AssemblyNameFlags.EnableJITcompileOptimizer)]
